0396b36640a3d307acc0f234e621e20feb52fb68
Jude Lilian <lilian.jude2@gmail.com>
2018-02-07 17:17:41 +0100

    evenements sur touches

--- Calculatrice/Calculatrice/MainWindow.xaml.cs
+++ Calculatrice/Calculatrice/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,109 @@ namespace Calculatrice
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new ViewModel();
+        }
+
+        private void Click_0(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "0";
+        }
+
+        private void Click_1(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "1";
+        }
+
+        private void Click_2(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "2";
+        }
+
+        private void Click_3(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "3";
+        }
+
+        private void Click_4(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "4";
+        }
+
+        private void Click_5(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "5";
+        }
+
+        private void Click_6(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "6";
+        }
+
+        private void Click_7(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "7";
+        }
+
+        private void Click_8(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "8";
+        }
+
+        private void Click_9(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "9";
+        }
+        private void Click_plus(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "+";
+        }
+        private void Click_moins(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "-";
+        }
+        private void Click_multiple(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "*";
+        }
+        private void Click_divise(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "/";
+        }
+        private void Click_parentheseG(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "(";
+        }
+        private void Click_parentheseD(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += ")";
+        }
+        private void Click_back(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += "";
+        }
+        private void Click_virgule(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result += ",";
+        }
+        private void Click_CE(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).Result = "";
+        }
+
+        private void Button_Click_Result(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).AddItem();
+        }
+
+        private void Button_Click_Clear_History(object sender, RoutedEventArgs e)
+        {
+            ((ViewModel)DataContext).MaListe.Clear();
         }
     }
 }
---
+++ Calculatrice/Calculatrice/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatrice
+{
+    public class ViewModel : BaseNotifyPropertyChanged
+    {
+
+        public string Result
+        {
+            get { return GetProperty<string>(); }
+            set { SetProperty(value); }
+        }
+
+        public ObservableCollection<string> MaListe
+        {
+            get { return GetProperty<ObservableCollection<string>>(); }
+            set { SetProperty(value); }
+        }
+
+        public string MonItemSelectionne
+        {
+            get { return GetProperty<string>(); }
+            set { SetProperty(value); }
+        }
+
+        public ViewModel()
+        {
+            Result = "";
+            MaListe = new ObservableCollection<string>();
+        }
+
+        private int cpt = 0;
+        public void AddItem()
+        {
+            cpt++;
+            MaListe.Add(Result);
+        }
+    }
+}
